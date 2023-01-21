2bdd45f8f3dc14d32bed03fedb51f20f6531bba4
Danil Buslaev <buslaev_d@garpix.com>
2023-01-21 19:27:53 +0300

    feat: add models

---
+++ src/top-page/top-page.model/top-page.model.ts
@@ -0,0 +1,25 @@
+export enum TopLevelCategory {
+  Courses,
+  Services,
+  Books,
+  Products,
+}
+
+export class TopPageModel {
+  firstCategory: TopLevelCategory;
+  secondCategory: string;
+  title: string;
+  hh?: {
+    count: number;
+    juniorSalary: number;
+    middleSalary: number;
+    seniorSalary: number;
+    advantages: {
+      title: string;
+      description: string;
+    }[];
+    seoText: string;
+    tagsTitle: string;
+    tags: string[];
+  };
+}
